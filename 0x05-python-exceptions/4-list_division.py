a3c739b5b3816d25edebff41eec759007abca95f
Karen Miano <mianokaren1798@gmail.com>
2023-08-28 13:02:01 -0700

    Performs element by element division in two lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,4 +13,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             list_result.append(0)
+        finally:
+            continue
     return (list_result)
