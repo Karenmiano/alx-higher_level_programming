2162fff8c6e86d2452d4eedf360410cdf2316383
Karen Miano <mianokaren1798@gmail.com>
2023-08-28 13:13:35 -0700

    Performs element by element division in two lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,12 +8,11 @@ def list_division(my_list_1, my_list_2, list_length):
             print("wrong type")
             list_result.append(0)
         except ZeroDivisionError:
-            print("division by zero")
+            print("division by 0")
             list_result.append(0)
         except IndexError:
             print("out of range")
             list_result.append(0)
-            break
         finally:
             continue
     return (list_result)
