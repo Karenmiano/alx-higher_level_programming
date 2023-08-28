81a5f5b6fb766089b30ddeb70a0641d828244059
Karen Miano <mianokaren1798@gmail.com>
2023-08-28 07:05:24 -0700

    Function that prints x elements of a list, handlesIndexError

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        i = 0
+        if x == 0:
+            return 0
+        while x:
+            print(my_list[i], end="")
+            x -= 1
+            i += 1
+        print()
+        return i
+    except IndexError:
+        print()
+        return i
