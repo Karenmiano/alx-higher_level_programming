933199513daf815397c7886e7eaadab22aaa71cd
Karen Miano <mianokaren1798@gmail.com>
2023-08-30 07:18:08 -0700

    class that defines a square by size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initializes a square
+
+        Args:
+            size: size of one side of square
+        """
+
+        self.__size = size
---
+++ 0x06-python-classes/trial.py
@@ -0,0 +1,4 @@
+class Geek:
+    def displayAge(self):
+        self.var = 10
+c = Gee
