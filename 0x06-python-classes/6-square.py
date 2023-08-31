69138d5dae62a08a3a7f83130b0fcc36f7c240e1
Karen Miano <mianokaren1798@gmail.com>
2023-08-30 19:54:37 -0700

    Square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+"""Defines a class Square"""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initializes a new square.
+
+        Args:
+            size(int): length of one side.
+        """
+        if isinstance(size, int) is not True:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Returns the size of a square"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the value of a square
+
+        Args:
+            value(int): size of square
+        """
+        if isinstance(value, int) is not True:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """Prints a visual square using #"""
+
+        if self.__size is 0:
+            print()
+        else:
+            for width in range(self.__size):
+                print("#" * self.__size)
+
+    def area(self):
+        """Returns the area of a square."""
+        return (self.__size ** 2)
