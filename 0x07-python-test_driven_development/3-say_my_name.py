433c3778c3253a7fc4f0938ef3542bdeab817a80
Karen Miano <mianokaren1798@gmail.com>
2023-09-03 09:19:25 -0700

    Test and function that prints full name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 
-def say_my_name(first_name, last_name=""):
+"""Defines the function say_my_name()"""
 
+
+def say_my_name(first_name, last_name=""):
+    """Prints names.
+
+    Args:
+       first_name: first name to print
+       last_name: last name to print
+    Raises:
+       TypeError: when input is not a string.
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
