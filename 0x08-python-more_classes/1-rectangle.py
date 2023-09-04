318eeafc2fd06992f8d2b4d137e75cba1635de01
Karen Miano <mianokaren1798@gmail.com>
2023-09-04 11:26:06 -0700

    Defining the measurements of my rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
+"""Defines a class Rectangle."""
+
 
 class Rectangle:
+    """Represents a rectangle."""
 
     def __init__(self, width=0, height=0):
+        """Creates a new square.
+
+        Args:
+            width: width of rectangle.
+            height: height of rectangle.
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Returns the width of rectangle"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Sets the width of Rectangle.
+
+        Args:
+            value: width of Rectangle.
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -20,10 +35,16 @@ class Rectangle:
 
     @property
     def height(self):
+        """Returns the height of the Rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Sets the height of the Rectangle.
+
+        Args:
+            value: height of the Rectangle.
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
