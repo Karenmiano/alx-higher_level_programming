48ef9b17d4450e33081f475af29c92bdd405c80f
Karen Miano <mianokaren1798@gmail.com>
2023-09-05 07:53:32 -0700

    Q14

--- 0x08-python-more_classes/1-main.py
+++ 0x08-python-more_classes/1-main.py
@@ -3,6 +3,6 @@ Rectangle = __import__('1-rectangle').Rectangle
 
 my_rectangle = Rectangle(2, 4)
 print(hex(id(my_rectangle)))
-my_rectangle2 = Rectangle(2, 4)
+my_rectangle2 = my_rectangle
 print(hex(id(my_rectangle2)))
 
