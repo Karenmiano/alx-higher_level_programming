fc6ef86d6557f34b4a5d2c49baf0344426e099be
Karen Miano <mianokaren1798@gmail.com>
2023-09-05 07:33:28 -0700

    Q1

--- 0x08-python-more_classes/1-main.py
+++ 0x08-python-more_classes/1-main.py
@@ -2,9 +2,7 @@
 Rectangle = __import__('1-rectangle').Rectangle
 
 my_rectangle = Rectangle(2, 4)
-print(my_rectangle.__dict__)
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle.__dict__)
+print(hex(id(my_rectangle)))
+my_rectangle2 = Rectangle(2, 4)
+print(hex(id(my_rectangle2)))
 
---
+++ 0x09-python-everything_is_object/README.md
@@ -0,0 +1,1 @@
+Python objects, mutable vs immutable objects. This directory will contain answers to quizzes asked on the behaviour of python objects.
