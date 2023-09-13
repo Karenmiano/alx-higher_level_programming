8b24ce9bf3abbcdd54178254d78c1dabfba474bb
Karen Miano <mianokaren1798@gmail.com>
2023-09-12 18:13:12 -0700

    Function that returns a python object from a json string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines the function from_json_string."""
+import json
+
+def from_json_string(my_str):
+    """Returns a python object from a json string."""
+    return json.loads(my_str)
