a2ccc64eb5c6cebe45e699af4ff474726f81e559
Karen Miano <mianokaren1798@gmail.com>
2023-09-13 03:26:43 -0700

    Script that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """Script that adds commandline arguments to a Python list
 and saves the list to a file."""
+if __name__ == '__main__':
 
-from sys import argv
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    from sys import argv
+    load_from_json_file = \
+        __import__("6-load_from_json_file").load_from_json_file
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
-try:
-    growing_list = argv[1:]
-    growing_list += load_from_json_file("add_item.json")
-    save_to_json_file(growing_list, "add_item.json")
-except FileNotFoundError:
-    save_to_json_file(growing_list, "add_item.json")
+    try:
+        growing_list = argv[1:]
+        growing_list += load_from_json_file("add_item.json")
+        save_to_json_file(growing_list, "add_item.json")
+    except FileNotFoundError:
+        save_to_json_file(growing_list, "add_item.json")
