96a3ff0567c63ba93ae2ec2cb379951ab7e83628
Karen Miano <mianokaren1798@gmail.com>
2023-10-17 12:11:48 -0700

    new and filled table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- creates a new table and inserts some records
+
+CREATE TABLE IF NOT EXISTS second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INTO second_table(id, name, score)
+VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(4, 'George', 8),
+(3, 'Bob', 14);
+SELECT * FROM second_table;
