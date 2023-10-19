d78b10b1ce1015a6e7bc7b0f5ddce8f43eb64e51
Karen Miano <mianokaren1798@gmail.com>
2023-10-19 12:28:58 -0700

    Foreign keys

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- create database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- create a table into data base
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+	id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
