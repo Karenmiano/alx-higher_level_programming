a07e6f88180289d7e6cb1383abfb4b5db6f4ded1
KarenMiano <mianokaren1798@gmail.com>
2023-11-13 12:00:02 -0800

    iterables

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const lines = ['C is Fun', 'Python is cool', 'Java is amazing'];
+const lines = ['C is fun', 'Python is cool', 'Java is amazing'];
 for (let line of lines) {
   console.log(line);
 }
