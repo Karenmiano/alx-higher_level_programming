96b6e9c061ef793f47e5598bf4ef80fa75aae6da
Karen Miano <mianokaren1798@gmail.com>
2023-11-18 13:57:23 -0800

    retrieve state by name

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """lists state id of object with the name passed as argument."""
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sys import argv
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
@@ -11,7 +11,8 @@ if __name__ == "__main__":
                               3306/{}""".format(argv[1], argv[2], argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
-    record = session.query(State).filter(State.name == (argv[4],)) \
+    record = session.query(State).filter(text("name=:name")) \
+                                 .params(name=argv[0]) \
                                  .order_by(State.id)
     try:
         print(record[0].id)
