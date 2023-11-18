128acf18e79be1d13e365a524b15e6f4425f31c8
Karen Miano <mianokaren1798@gmail.com>
2023-11-18 10:47:20 -0800

    list records

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,9 +5,9 @@ from sys import argv
 from model_state import State
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("""mysql://{}:{}@localhost:3306/{}""".format(argv[1],
-                          argv[2], argv[3]))
-Session = sessionmaker(bind = engine)
+engine = create_engine("""mysql://{}:{}@localhost:
+                          3306/{}""".format(argv[1], argv[2], argv[3]))
+Session = sessionmaker(bind=engine)
 session = Session()
 records = session.query(State).all()
 for record in records:
