b923dc4a69f8ac3164768e93cb8884878b00f1a0
Karen Miano <mianokaren1798@gmail.com>
2023-11-18 11:02:07 -0800

    list records

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,7 +8,6 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine("""mysql://{}:{}@localhost:
                               3306/{}""".format(argv[1], argv[2], argv[3]))
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     records = session.query(State).all()
