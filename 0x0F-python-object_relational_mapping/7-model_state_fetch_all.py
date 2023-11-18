a4fd8fe4fb026f9e686aa3cef52a8a906d5b5ae8
Karen Miano <mianokaren1798@gmail.com>
2023-11-18 11:13:08 -0800

    list records

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """lists all State objects from the database hbtn_0e_6_usa."""
 
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sys import argv
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
