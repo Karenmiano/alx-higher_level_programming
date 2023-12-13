d3585b63aad6819deebde3608800506bb1eff0dc
Karenmiano <mianokaren1798@gmail.com>
2023-12-13 14:34:49 +0300

    custom character

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,11 +3,11 @@
 const Square1 = require('./5-square');
 
 class Square extends Square1 {
-    charPrint (c = 'X') {
-        const sym = c.repeat(this.width);
-        for (let i = 1; i <= this.height; i++) {
-            console.log(sym);
-        }
-    } 
+  charPrint (c = 'X') {
+    const sym = c.repeat(this.width);
+    for (let i = 1; i <= this.height; i++) {
+      console.log(sym);
+    }
+  }
 }
 module.exports = Square;
