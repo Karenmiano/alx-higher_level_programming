1e2520331b00ff06f02482672618d9359103f1e0
Karenmiano <mianokaren1798@gmail.com>
2023-12-15 23:59:23 +0300

    case sensitive search

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,2 +1,22 @@
-import sys
-print(sys.argv)
+#!/usr/bin/python3
+"""lists all states from database starting with N"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        port=3306
+    )
+    cur = db.cursor()
+    cur.execute("""SELECT * from states WHERE BINARY name LIKE "N%"
+                ORDER BY id ASC""")
+    results = cur.fetchall()
+    for record in results:
+        print(record)
+    cur.close
+    db.close
