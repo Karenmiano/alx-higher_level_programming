288bcf43a5ade63470e7bbd2a3f7532dff0609b1
Karenmiano <mianokaren1798@gmail.com>
2023-12-16 13:55:02 +0300

    cities from state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""lists cities from database belonging to partcular state"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        port=3306
+    )
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM cities
+                JOIN states
+                ON states.id = cities.state_id
+                WHERE states.name = %s""", (argv[4], ))
+    results = cur.fetchall()
+    cities = [record[0] for record in results]
+    print(' ,'.join(cities))
+    cur.close()
+    db.close()
