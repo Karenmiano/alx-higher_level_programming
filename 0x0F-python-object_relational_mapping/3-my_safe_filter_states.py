c226cd0da1e989c788e5001879669df19f03fb77
Karenmiano <mianokaren1798@gmail.com>
2023-12-16 11:49:48 +0300

    injection safe

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""lists state from database where name is Arizona"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        port=3306
+    )
+    cur = db.cursor()
+    cur.execute("""SELECT * from states WHERE BINARY name = %s
+                ORDER BY id ASC""", (argv[4],))
+    results = cur.fetchall()
+    for record in results:
+        print(record)
+    cur.close()
+    db.close()
+    
