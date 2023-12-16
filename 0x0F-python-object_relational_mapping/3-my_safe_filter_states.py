b4cf60ff69b49a8cce77576853bdfa65f5c4a7a3
Karenmiano <mianokaren1798@gmail.com>
2023-12-16 12:11:23 +0300

    injection safe

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     )
     cur = db.cursor()
     cur.execute("""SELECT * FROM states WHERE BINARY name = %s
-                ORDER BY id ASC""", (argv[4],))
+                ORDER BY id ASC""", (argv[4], ))
     results = cur.fetchall()
     for record in results:
         print(record)
