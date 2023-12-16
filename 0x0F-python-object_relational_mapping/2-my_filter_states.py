0eda4706b3246bd728917af2dfbbc5cf250c494f
Karenmiano <mianokaren1798@gmail.com>
2023-12-16 10:20:52 +0300

    select Arizona

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""lists state from database where name is Arizona"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        port=3306
+    )
+    cur = db.cursor()
+    cur.execute("""SELECT * from states WHERE name = '{}'
+                ORDER BY id ASC""".format(argv[4]))
+    results = cur.fetchall()
+    for record in results:
+        print(record)
+    cur.close
+    db.close
