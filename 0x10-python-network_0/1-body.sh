82473a358a107b257576356b43627007941f5b22
Karenmiano <mianokaren1798@gmail.com>
2024-01-05 22:29:44 +0300

    display response body only if status 200 OK

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # receives url, sends GET request and displays bodu of response only if status code is 200 OK
-curl -s -o save -X GET -w "%{http_code}" "$1" | grep -q "200" && sudo cat save
+curl -s -o save -X GET -w "%{http_code}" "$1" | grep -q "200" && cat save
