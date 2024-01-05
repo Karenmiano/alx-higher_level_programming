c5db8e2360951c8905a51bc34535c03a01d48b0d
Karenmiano <mianokaren1798@gmail.com>
2024-01-05 19:40:14 +0300

    body size of response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# script that accepts a URL, sends a request to the URL and
-# displays the size of the body of the response
-
-if [[ -n $1 ]]
-then
-    content_length=$(curl -sI "$1" | grep -oP "^Content-Length: \K\d+")
-    echo "$content_length"
-fi
+# script that accepts a URL, sends a request to the URL and displays the size of the body of the response
+curl -sI "$1" | grep -oP "^Content-Length: \K\d+"
