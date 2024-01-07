6b082baaea0d15c89f216e36cd40b3e7b665f860
Karenmiano <mianokaren1798@gmail.com>
2024-01-07 20:18:02 +0300

    get header value

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,10 +4,10 @@ Takes in url and an email address, sends post request
 """
 
 if __name__ == '__main__':
-  import requests
-  import sys
+    import requests
+    import sys
 
-  url = sys.argv[1]
-  value = {'email': sys.argv[2]}
-  res = requests.post(url, data=value)
-  print(res.text)
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+    res = requests.post(url, data=value)
+    print(res.text)
