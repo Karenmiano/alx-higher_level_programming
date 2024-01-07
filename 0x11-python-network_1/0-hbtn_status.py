84302ac5bec702dc6e169222e237b89106ea1fbd
Karenmiano <mianokaren1798@gmail.com>
2024-01-07 14:28:12 +0300

    format response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """Fetches content from link and displays type,
 binary version and decoded version"""
-import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    ans = response.read()
-response_type = type(ans)
-decoded = ans.decode('utf-8')
-print(f"Body response:\n"
-      f"\t- type: {response_type}\n"
-      f"\t- content: {ans}\n"
-      f"\t- utf8 content: {decoded}")
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        ans = response.read()
+    response_type = type(ans)
+    decoded = ans.decode('utf-8')
+    print(f"Body response:\n"
+          f"\t- type: {response_type}\n"
+          f"\t- content: {ans}\n"
+          f"\t- utf8 content: {decoded}")
