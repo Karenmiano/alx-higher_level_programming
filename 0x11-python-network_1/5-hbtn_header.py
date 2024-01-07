42efc05ca7e7f6f9aa10cc594b0897ec31f24da7
Karenmiano <mianokaren1798@gmail.com>
2024-01-07 18:34:49 +0300

    get header val

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """Takes in url, makes request and fetches value of X-Request-Id
 from response headers"""
+import requests
+import sys
 
-if __name__ == '__main__':
-    import requests
-    import sys
-
+if __name__ == '__main__':    
     res = requests.get(sys.argv[1])
     val = res.headers.get('X-Request-Id')
     print(val)
