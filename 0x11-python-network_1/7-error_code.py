e8b5c3726680b597cb0d08e6cd136d0ae19fddda
Karenmiano <mianokaren1798@gmail.com>
2024-01-07 20:48:58 +0300

    check error codes before printing

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     response = requests.get(sys.argv[1])
     status_code = response.status_code
     if status_code >= 400:
-        print(f'Error code:{status_code}')
+        print(f'Error code: {status_code}')
     else:
         print(response.text)
