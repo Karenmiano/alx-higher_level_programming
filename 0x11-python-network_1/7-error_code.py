de8be05e0dc7dbcbf1debded299ce9d766ef0445
Karenmiano <mianokaren1798@gmail.com>
2024-01-07 20:41:19 +0300

    check error codes before printing

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""takes a url and prints body depending on status code"""
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    response = requests.get(sys.argv[1])
+    status_code = response.status_code
+    if status_code >= 400:
+        print(f'Error code:{status_code}')
+    else:
+        print(response.text)
