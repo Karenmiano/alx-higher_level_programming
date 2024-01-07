50ec7db5ab91c15a5ee732c9380f7d93227ac185
Karenmiano <mianokaren1798@gmail.com>
2024-01-07 23:51:52 +0300

    fetch json data

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""takes in letter, makes request to url and anticipates json data"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    letter = "" if len(sys.argv) != 2 else sys.argv[1]
+    value = {'q': letter}
+    url = 'http://0.0.0.0:5000/search_user'
+    try:
+        res = requests.post(url, data=value)
+        json_decoded = res.json()
+        if len(json_decoded) == 0:
+            print("No result")
+        else:
+            id = json_decoded['id']
+            name = json_decoded['name']
+            print(f'[{id}] {name}')
+    except json.decoder.JSONDecodeError:
+        print("Not a valid JSON")
